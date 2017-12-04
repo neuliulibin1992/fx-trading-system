b12263b42d816b3310f194117a31fc20df8e0fa6
smaxll <smaxllimit@gmail.com>
2017-12-04 22:15:56 +0800

    Update code-gen scripts

--- devops-scripts/setup-entities.sh
+++ devops-scripts/setup-entities.sh
@@ -2,8 +2,7 @@
 
 #Define the folders in which we will run `yo jhipster:import-jdl entities.jh`
 
-apps=("gateway" 
-"fx-price-service")
+apps=("fx-price-service")
 for app in "${apps[@]}";
 do
     ( cd ../$app && jhipster import-jdl entities.jh )
